ec3f54f5c0e295abeeffe2f5bc66af7629c1f804
Kamil Krzysztof <kamil.krzysztof@miquido.com>
2020-11-07 11:52:02 +0100

    init

---
+++ app/src/main/java/com/programistamobile/mvvmapp/FilmsActivity.kt
@@ -0,0 +1,11 @@
+package com.programistamobile.mvvmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilmsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_films)
+    }
+}
